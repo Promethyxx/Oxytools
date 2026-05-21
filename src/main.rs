a7e149781c9937ca5797e9247f66b7d623f25526
Promethyxx <peter.mboyo@ik.me>
2026-05-21 12:33:18 +0200

    delete terminal in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![windows_subsystem = "windows"]
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub mod lang;
 pub mod logging;
